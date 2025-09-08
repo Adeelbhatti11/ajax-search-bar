b6ddffd74a16c90a79010da375d7e5e4954a29d1
Adeelbhatti11 <adeelbhattiab123@gmail.com>
2025-09-08 19:36:02 +0500

    search bar

---
+++ search.php
@@ -0,0 +1,32 @@
+<?php
+$search = $_POST['Search'];
+$conn = mysqli_connect("localhost", "root", "", "ajax") or die("Connection failed");
+
+// Escape special characters to prevent SQL Injection
+$search = mysqli_real_escape_string($conn, $search);
+
+// Use LIKE for flexible search
+$sql = "SELECT * FROM users WHERE name LIKE '%$search%'";
+$result = mysqli_query($conn, $sql) or die("Query Failed.");
+
+$output = "";
+
+if(mysqli_num_rows($result) > 0){
+    while($row = mysqli_fetch_assoc($result)){
+        $output .= "<tr>
+                        <td>{$row['id']}</td>
+                        <td>{$row['name']}</td>
+                        <td>{$row['email']}</td>
+                        <td style='width: 100px;'>
+                            <input type='button' class='edit-btn' style='margin: 5px 0px; color: green; border: 1px solid green' data-id='{$row['id']}' value='Edit'>
+                            <input type='button' class='del-btn' style='color: red; border: 1px solid red' data-id='{$row['id']}' data-name='{$row['name']}' value='Delete'>
+                        </td>
+                    </tr>";
+    }
+    echo $output;
+} else {
+    echo "<tr><td colspan='4'>No users found</td></tr>";
+}
+
+mysqli_close($conn);
+?>
